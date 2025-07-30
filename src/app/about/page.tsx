6510db1cfa77823a62d8bc696cbd79f64773b46d
Erica <Erica@ip-192-168-1-4.eu-west-3.compute.internal>
2025-07-29 20:41:15 -0400

    Center activities.png image on About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,19 +9,21 @@ export default function About() {
 
       {/* Festival Description Section */}
       <section className="py-16 px-4 bg-[#EDDEB7]">
-        <div className="max-w-4xl mx-auto">
-          <div className="space-y-8 text-center">
-            <p className="text-lg md:text-xl leading-relaxed font-sans text-[#566E37]">
+        <div className="max-w-6xl mx-auto">
+          <div className="space-y-4 text-center">
+            <p className="text-lg md:text-xl leading-relaxed font-sans text-[#245451]">
               Flights & Sounds is a festival fostering connection and community through music, art, nature, and performance. We aim to provide experiences and activities that bring joy and inspire creativity.
             </p>
             
-            <p className="text-lg md:text-xl leading-relaxed font-sans text-[#566E37]">
+            <p className="text-lg md:text-xl leading-relaxed font-sans text-[#245451]">
               Our festival is built from the ground up, with the creators being musicians and performers themselves. Any profits made will be split among the artists and performers who participate, as well as given to charity organizations.
             </p>
           </div>
         </div>
-        <br></br>
-        <img src="/activities.png" alt="Activities at Flights & Sounds" className="w-1/2 h-auto" />
+        <br></br><br></br>
+        <div className="flex justify-center">
+          <img src="/activities.png" alt="Activities at Flights & Sounds" className="w-1/2 h-auto" />
+        </div>
       </section>
 
       {/* Activities Image */}
